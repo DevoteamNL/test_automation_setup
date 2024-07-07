509e531641d239dc9149a894b2dfabb940ee649c
alandevoteam <alan.ahmad@devoteam.com>
2024-07-07 19:24:48 +0200

    update version inspector

--- inspector/start.bat
+++ inspector/start.bat
@@ -1,4 +1,4 @@
 wsl docker run --rm --name inspector^
     -e DISPLAY=:0 ^
     -v /tmp/.X11-unix:/tmp/.X11-unix ^
-    mcr.microsoft.com/playwright:v1.43.0 npx -y playwright open google.com
+    mcr.microsoft.com/playwright:v1.45.1 npx -y playwright open google.com
