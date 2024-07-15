eac8b937cf1cc816eeb1a35fc3436b2f84284968
Jeroen van der Meulen <jeroennathan.vandermeulen@gmail.com>
2024-07-15 22:52:33 +0200

    Update test script

--- runner/mount/test_run.py
+++ runner/mount/test_run.py
@@ -3,12 +3,12 @@ from playwright.sync_api import Page, expect
 
 
 def test_example(page: Page) -> None:
-    page.goto("https://myprivacy.dpgmedia.nl/consent?siteKey=ucf98legs1caotgh&callbackUrl=https%3A%2F%2Fwww.nu.nl%2Fprivacy-gate%2Faccept%3FredirectUri%3D%252F")
-    page.frame_locator("iframe[title=\"SP Consent Message\"]").get_by_label("Akkoord").click()
-    page.get_by_label("sluit venster").click()
-    page.get_by_role("link", name="Astronomen zijn erachter").first.click()
-    page.get_by_label("Hoofdnavigatie").get_by_label("Sport").click()
-    page.get_by_role("link", name="Barcelona-trainer Xavi").first.click()
+    page.goto("https://www.booking.com/")
+    page.get_by_placeholder("Where are you going?").click()
+    page.get_by_role("button", name="Texel Netherlands").click()
+    page.get_by_label("24 August").click()
+    page.get_by_label("31 August").click()
+    page.get_by_role("button", name="Search").click()
     with page.expect_popup() as page1_info:
-        page.get_by_alt_text("Barcelona-trainer Xavi").click()
+        page.get_by_role("link", name="Landgoed Hotel Tatenhove").click()
     page1 = page1_info.value
