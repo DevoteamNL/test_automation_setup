c71ef74ae290a361c1ac24e6b0abedbf354923e8
Jeroen van der Meulen <jeroennathan.vandermeulen@gmail.com>
2025-01-27 14:03:20 +0000

    Script for installing docker

---
+++ scripts/install-docker.sh
@@ -0,0 +1,17 @@
+!#/bin/bash
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add yourself to the docker group
+sudo usermod -aG docker $USER
+
+# Exit bash - this is important!!!
+exit
+
+# Afterwards close all WSL terminals and execute command --wsl --shutdown in PowerShell.
